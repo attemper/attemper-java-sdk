c29e02c1f1d4912393e5ee80dc082dd1a9026875
ldang264 <820704815@qq.com>
2019-09-03 19:55:19 +0800

    remove data map

--- attemper-java-sdk-common/src/main/java/com/github/attemper/java/sdk/common/result/execution/TaskResult.java
+++ attemper-java-sdk-common/src/main/java/com/github/attemper/java/sdk/common/result/execution/TaskResult.java
@@ -9,26 +9,12 @@ public class TaskResult extends LogResult {
      */
     protected Map<String, Object> paramMap;
 
-    /**
-     * in the task scope
-     */
-    protected Map<String, Object> dataMap;
-
     public Map<String, Object> getParamMap() {
         return paramMap;
     }
 
     public TaskResult setParamMap(Map<String, Object> paramMap) {
         this.paramMap = paramMap;
-        return this;
-    }
-
-    public Map<String, Object> getDataMap() {
-        return dataMap;
-    }
-
-    public TaskResult setDataMap(Map<String, Object> dataMap) {
-        this.dataMap = dataMap;
         return this;
     }
 }
